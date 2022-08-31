27f80c9ca3cbd82ec5ea9e9d4baa6cad42f3bf30
sumqwerty <sumit999saini@gmail.com>
2022-09-01 01:24:00 +0530

    adding checkbox toggles

--- src/arrow.js
+++ src/arrow.js
@@ -5,8 +5,10 @@ class Arrow{
         this.id = _id;
         this.magnitude = _magnitude;
         this.angle = _angle;
-        this.xComponent = _magnitude * this.sketch.cos(this.sketch.radians(_angle));
-        this.yComponent = _magnitude * this.sketch.sin(this.sketch.radians(_angle));
+        // this.xComponent = Math.floor(_magnitude * this.sketch.cos(this.sketch.radians(_angle)));
+        // this.yComponent = Math.floor(_magnitude * this.sketch.sin(this.sketch.radians(_angle)));
+        this.xComponent = (_magnitude * this.sketch.cos(this.sketch.radians(_angle)));
+        this.yComponent = (_magnitude * this.sketch.sin(this.sketch.radians(_angle)));
         this.x = _x;
         this.y = _y;
         this.x2 = this.x+this.xComponent;
@@ -17,7 +19,10 @@ class Arrow{
         this.inRangeToMove = this.minRange; 
         this.controlParent = document.getElementById(_controlContainer);
         this.color = this.sketch.color(this.sketch.random(0,255),this.sketch.random(0,255),this.sketch.random(0,255));
+        this.showComponents = false;
+        
         this.createControl();
+
     }
 
     createControl(){
@@ -26,8 +31,8 @@ class Arrow{
         outerDiv.style.display = "flex";
         outerDiv.style.flexDirection = "column";
 
-        this.magSlider = new scrollRange('Magnitude',10,1.5,0,500,this.magnitude,this.color);
-        this.angleSlider = new scrollRange('Angle',10,1.5,0,360,this.angle,this.color);
+        this.magSlider = new scrollRange('Magnitude',10,1.5,0,500,this.magnitude,this.color,'after');
+        this.angleSlider = new scrollRange('Angle',10,1.5,0,360,this.angle,this.color,'after');
         
         let closeButton = document.createElement('div');
         closeButton.classList.add('close-button');
@@ -36,23 +41,164 @@ class Arrow{
         outerDiv.appendChild(this.angleSlider.getRanger());
         this.controlParent.appendChild(outerDiv);
 
+        // div to display x component and other stuff
+        let dispXcomp = document.createElement('div');
+        dispXcomp.style.display = 'flex';
+        dispXcomp.style.flexDirection = 'row';
+        dispXcomp.classList.add('infoPanelText');
+
+        this.xCompSld = new scrollRange('X:',9,1.5,-500,500,this.magnitude,this.color,'before');
+        dispXcomp.appendChild(this.xCompSld.getRanger());
+        outerDiv.appendChild(dispXcomp);
+
+
+        let dispYcomp = document.createElement('div');
+        dispYcomp.style.display = 'flex';
+        dispYcomp.style.flexDirection = 'row';
+        dispYcomp.classList.add('infoPanelText');
+
+        this.yCompSld = new scrollRange('Y:',9,1.5,-500,500,0,this.color,'before');
+        dispYcomp.appendChild(this.yCompSld.getRanger());
+        outerDiv.appendChild(dispYcomp);
+        outerDiv.appendChild(dispYcomp);
+
+
+        let toggleComponentContainer = document.createElement('div');
+        toggleComponentContainer.classList.add('checkBoxContainer');
+        this.componentSwitch = document.createElement('input');
+        this.componentSwitch.type = 'checkbox';
+        this.componentSwitch.id = this.id+'cSwitch';
+        this.componentSwitch.style.height = '1.5rem';
+        this.componentSwitch.style.margin = '2px';
+        
+        let cSwitchLabel = document.createElement('label');
+        cSwitchLabel.innerHTML = 'Components';
+        cSwitchLabel.htmlFor = this.componentSwitch.id;
+        cSwitchLabel.style.height = '1.5rem';
+        toggleComponentContainer.appendChild(this.componentSwitch);
+        toggleComponentContainer.appendChild(cSwitchLabel);
+        dispXcomp.appendChild(toggleComponentContainer);
+
+
+        let toggleAdd = document.createElement('div');
+        toggleAdd.classList.add('checkBoxContainer');
+        toggleAdd.style.marginRight = '0.5rem';
+        this.addSwitch = document.createElement('input');
+        this.addSwitch.type = 'checkbox';
+        this.addSwitch.id = this.id+'addSwitch';
+        this.addSwitch.style.height = '1.5rem';
+        this.addSwitch.style.margin = '2px';
+
+        let aSwitchLabel = document.createElement('label');
+        aSwitchLabel.innerHTML = 'Add';
+        aSwitchLabel.htmlFor = this.addSwitch.id;
+        aSwitchLabel.style.height = '1.5rem';
+        toggleAdd.appendChild(this.addSwitch);
+        toggleAdd.appendChild(aSwitchLabel);
+        dispYcomp.appendChild(toggleAdd);
+
+        let toggleSub = document.createElement('div');
+        toggleSub.classList.add('checkBoxContainer');
+        toggleSub.style.marginLeft = '0.5rem';
+        this.subSwitch = document.createElement('input');
+        this.subSwitch.type = 'checkbox';
+        this.subSwitch.id = this.id+'subSwitch';
+        this.subSwitch.style.height = '1.5rem';
+        this.subSwitch.style.margin = '2px';
+
+        let sSwitchLabel = document.createElement('label');
+        sSwitchLabel.innerHTML = 'Sub';
+        sSwitchLabel.htmlFor = this.subSwitch.id;
+        sSwitchLabel.style.height = '1.5rem';
+        toggleSub.appendChild(this.subSwitch);
+        toggleSub.appendChild(sSwitchLabel);
+        dispYcomp.appendChild(toggleSub);
+
+
+
+
+        
+        
+
         outerDiv.classList.add("outer-div");
 
-
         let obj = this;
+
         // this will call the deleteThis function of the sketch object defiend in the script.js file
         closeButton.addEventListener('click',(event) => {
             obj.sketch.deleteThis(obj.id);
         });
 
+        this.magSlider.getSlider().addEventListener('input',(event)=>{
+            obj.updateMagnitude();
+        });
+
+        this.angleSlider.getSlider().addEventListener('input',(event)=>{
+            obj.updateAngle();
+        });
+
+        this.xCompSld.getSlider().addEventListener('input',(event)=>{
+            obj.updateXComponent();
+        });
+
+        this.yCompSld.getSlider().addEventListener('input',(event)=>{
+            obj.updateYComponent();
+        });
+
+        this.componentSwitch.addEventListener('input', (event)=> {
+            obj.showComponents = event.target.checked;
+        });
+
+
     }
 
-    updateAngle(angl){
-        this.angle = angl;
+    updateXComponent(){
+        this.xComponent = this.xCompSld.value;
+        this.manualUpdateMagAngle();
     }
 
-    updateMagnitude(mag){
-        this.magnitude = mag;
+    updateYComponent(){
+        this.yComponent = -this.yCompSld.value;
+        this.manualUpdateMagAngle();
+    }
+
+    updateAngle(){
+        this.angle = -1 * this.angleSlider.value;
+        this.manualUpdateComponents();
+    }
+
+    updateMagnitude(){
+        this.magnitude = this.magSlider.value;
+        this.manualUpdateComponents();
+    }
+
+    manualUpdateMagAngle(){
+        let temp = this.sketch.createVector(this.xComponent, this.yComponent);
+        this.magnitude = Math.floor(temp.mag());
+        
+        let unitX = this.sketch.createVector(1,0);
+        
+        let tempA = Math.floor(this.sketch.degrees(unitX.angleBetween(temp)));
+        if(tempA < 0)this.angle = -1 * tempA;
+        else this.angle = 360 - tempA;
+        
+
+        this.magSlider.updateSlider(this.magnitude);
+        this.angleSlider.updateSlider(this.angle);
+    }
+
+    manualUpdateComponents(){
+        // this.xComponent = Math.floor(this.magnitude * this.sketch.cos(this.sketch.radians(this.angle)));
+        // this.yComponent = Math.floor(this.magnitude * this.sketch.sin(this.sketch.radians(this.angle)));
+        
+        this.xComponent = (this.magnitude * this.sketch.cos(this.sketch.radians(this.angle)));
+        this.yComponent = (this.magnitude * this.sketch.sin(this.sketch.radians(this.angle)));
+        
+        if(Math.abs(this.angle) == 90 || Math.abs(this.angle) == 270    )this.xComponent = 0;
+        else if(this.angle == 0 || this.angle == -180 || this.angle == -360)this.yComponent = 0;
+
+        this.xCompSld.updateSlider(Math.floor(this.xComponent));
+        this.yCompSld.updateSlider(Math.floor(-this.yComponent));
     }
 
     deleteControls(){
@@ -60,10 +206,6 @@ class Arrow{
     }
 
     update(){
-        this.magnitude = this.magSlider.value
-        this.angle = -1 * this.angleSlider.value;
-        this.xComponent = this.magnitude * this.sketch.cos(this.sketch.radians(this.angle));
-        this.yComponent = this.magnitude * this.sketch.sin(this.sketch.radians(this.angle));
         this.x2 = this.x+this.xComponent;
         this.y2 = this.y+this.yComponent;
 
@@ -90,13 +232,49 @@ class Arrow{
         }
     }
 
+    drawHead(c1,c2,xcomp,ycomp){
+        let a1 = this.sketch.createVector(xcomp,ycomp);
+        a1.setMag(15);
+        this.sketch.push();
+        this.sketch.translate(c1,c2);
+        this.sketch.rotate(this.sketch.radians(-135));
+        this.sketch.line(0,0,a1.x,a1.y);
+        this.sketch.rotate(this.sketch.radians(135));
+        this.sketch.rotate(this.sketch.radians(135));
+        this.sketch.line(0,0,a1.x,a1.y);
+        this.sketch.rotate(this.sketch.radians(-135));
+        this.sketch.pop();
+    }
+
     disp(){
+        
         this.sketch.fill(this.color);
         this.sketch.stroke(this.color);
-        this.sketch.strokeWeight(2);
+        this.sketch.strokeWeight(5);
         this.sketch.circle(this.x,this.y,10);
         this.sketch.line(this.x,this.y,this.x2,this.y2);
+        this.drawHead(this.x2,this.y2,this.xComponent,this.yComponent);
+        if(this.showComponents){
+            // x component
+            if(this.angle != 270 || this.angle != 90){
+                this.sketch.push();
+                this.sketch.drawingContext.setLineDash([15, 10]);
+                this.sketch.line(this.x,this.y,this.x2,this.y);
+                this.drawHead(this.x2,this.y,this.xComponent,0);
+                this.sketch.pop();
+            }
+            // y component
+            if(this.angle != 0 || this.angle != 180 || this.angle != 360){
+                this.sketch.push();
+                this.sketch.drawingContext.setLineDash([15, 10]);
+                this.sketch.line(this.x,this.y,this.x,this.y2);
+                this.drawHead(this.x,this.y2, 0,this.yComponent);
+                this.sketch.pop();
+            }
+        }
     }
+
+
 
 
     toolTip(){
--- src/slider.js
+++ src/slider.js
@@ -3,7 +3,7 @@ Number.prototype.map = function (in_min, in_max, out_min, out_max) {
 }
 
 class scrollRange{
-    constructor(_name, _width, _height, _min, _max, _value, _color){
+    constructor(_name, _width, _height, _min, _max, _value, _color, _namePos){
         this.width = _width;
         this.height = _height;
         this.min = _min;
@@ -11,11 +11,11 @@ class scrollRange{
         this.value = _value;
         this.color = _color;
         this.name = _name;
+        this.namePos = _namePos;
         
     }
 
     getRanger(){
-
         this.container = document.createElement('div');
         this.container.style.display = 'flex';
         this.container.style.flexDirection = 'row';
@@ -38,6 +38,7 @@ class scrollRange{
         this.innerRect.style.width = this.value.map(this.min, this.max, 0, this.width)+'rem';
         this.innerRect.style.height = this.height+'rem';
         this.innerRect.style.backgroundColor = this.color;
+        this.innerRect.style.maxWidth = this.max.map(this.min, this.max, 0, this.width)+'rem';
         this.outer.appendChild(this.innerRect);
 
         this.rValue = document.createElement('div');
@@ -46,25 +47,37 @@ class scrollRange{
         this.rValue.style.fontSize = this.height-(this.height/4)+'rem';
         this.outer.appendChild(this.rValue);
 
-        this.container.appendChild(this.outer);
-
         this.rLabel = document.createElement('span');
         this.rLabel.innerHTML = this.name;
         this.rLabel.style.fontSize = this.height-(this.height/4)-0.1+'rem';
         this.rLabel.style.alignSelf = 'center';
-        this.container.appendChild(this.rLabel);
 
+        if(this.namePos == 'after'){
+            this.container.appendChild(this.outer);
+            this.container.appendChild(this.rLabel);
+        }
+        else{
+            this.container.appendChild(this.rLabel);
+            this.container.appendChild(this.outer);
+        }
         
         
         let obj = this;
         this.range.addEventListener('input',function(){
-            obj.value = parseInt(this.value);
-            obj.innerRect.style.width = obj.value.map(obj.min, obj.max, 0, obj.width)+'rem';
-            obj.rValue.innerHTML = this.value;
+            obj.updateSlider(this.value);
         });
 
         return this.container;
     }
 
+    updateSlider(val){
+        this.value = parseInt(val);
+        this.innerRect.style.width = this.value.map(this.min, this.max, 0, this.width)+'rem';
+        this.rValue.innerHTML = val;
+    }
+
+    getSlider(){
+        return this.range;
+    }
 
 }
